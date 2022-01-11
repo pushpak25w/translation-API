55788d9389b617b42b96b51a2e6f2062e975c400
Pushpak <50488701+pushpak25w@users.noreply.github.com>
2022-01-11 11:37:44 +0530

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-import requests
-BASE="http://127.0.0.1:6000/"
-response=requests.put(BASE+"category/test",{'lang':'english','trans':'marathi','sentence':'give it to me'})
-data=response.json()
-print(data)
